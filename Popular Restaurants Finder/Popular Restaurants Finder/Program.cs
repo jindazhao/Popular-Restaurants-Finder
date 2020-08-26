6421ad16c2be00bb34dbb19cd7a49d0bc75a6ac6
jindaboi <jindazhao01@gmail.com>
2020-08-26 12:59:58 -0700

    Added method with API to get respone for restaurants based on longitude and latitudes

---
+++ Popular Restaurants Finder/Popular Restaurants Finder/Program.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Popular_Restaurants_Finder
+{
+    class Program
+    {
+        public static string location;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_____________________________________________________");
+            Console.WriteLine("Hello! Welcome to Jinda's Popular Restaurant Finder!");
+            Console.WriteLine("_____________________________________________________");
+            Console.WriteLine("_____________________________________________________");
+            Console.WriteLine("                                                     ");
+
+            Console.Write("Enter the desired location: ");
+            location = Console.ReadLine();
+
+
+            Console.ReadLine();
+        }
+
+        static void FindRestaurants(float lat, float lang)
+        {
+            var client = new RestClient("https://appetitoso-best-food-dishes-v1.p.rapidapi.com/dish/suggestions/?radius=15&lang=it&lat=" + lat + "&lng=" + lang);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "appetitoso-best-food-dishes-v1.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "2c55019311msha703d4f4d477ac6p1b0a95jsn20b3f6f65c02");
+            IRestResponse response = client.Execute(request);
+            //https://rapidapi.com/appetitoso/api/food-search-engine?endpoint=55cdb48ce4b0b74f06700640
+
+
+        }
+
+        static void convertLocation(string location)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Popular Restaurants Finder/Popular Restaurants Finder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Popular Restaurants Finder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Popular Restaurants Finder")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56f73805-cc15-45db-a2e8-0b63da9ae86a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
